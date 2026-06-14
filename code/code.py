7b28d7104c91b1b13153177eee8a6e55af08d27e
Caio Eduardo <cadducaio@gmail.com>
2026-06-14 16:54:27 -0300

    Update code.py

--- code/code.py
+++ code/code.py
@@ -1,3 +1,8 @@
-
+from colorama import Fore, Back, Style
 def capa():
-    print()
+    titulo = "C H A T  T E R R Y  V 1"
+
+    print("╔════════════════════════════╗")
+    print(Fore.WHITE + Back.BLUE + "║" + titulo.center(28) + "║" + Style.RESET_ALL)
+    print("╚════════════════════════════╝")
+capa()
